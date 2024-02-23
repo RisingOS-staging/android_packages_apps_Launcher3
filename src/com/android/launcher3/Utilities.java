9a75cfd244e48be69ea86ca186d75b5262359083
SlowNicoFish <marlonn.dev@proton.me>
2024-02-23 08:52:21 +0100

    Launcher3: Add an option to hide/show scrollbar in app drawer
    neobuddy89: Clean up and adapt for Launcher3.
    
    Co-authored-by: Zongle Wang <wangzongler@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -197,6 +197,7 @@ public final class Utilities {
     public static final String KEY_SHOW_QUICKSPACE_WEATHER_CITY = "pref_quickspace_weather_city";
     public static final String KEY_SHOW_QUICKSPACE_WEATHER_TEXT = "pref_quickspace_weather_text";
     public static final String KEY_FORCE_MONOCHROME_ICONS = "pref_forced_monochrome_icons";
+    public static final String KEY_DRAWER_SCROLLBAR = "pref_drawer_scrollbar";
 
     /**
      * Returns true if theme is dark.
@@ -1096,5 +1097,10 @@ public final class Utilities {
     public static boolean enableMonoChromeThemedIcons(Context context) {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_FORCE_MONOCHROME_ICONS, false);
+    }
+
+    public static boolean showScrollbar(Context context) {
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_DRAWER_SCROLLBAR, true);
     }
 }
